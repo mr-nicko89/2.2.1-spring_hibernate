08374cb69b74dfd7393f8ed9b084e474262f2fa1
Nickolay <mr-nicko89@yandex.ru>
2021-04-05 22:18:47 +0300

    Настроил вывод model и series при выводе списка пользователей.

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,7 +1,6 @@
 package hiber;
 
 import hiber.config.AppConfig;
-import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,11 +14,10 @@ public class MainApp {
 
         UserService userService = context.getBean(UserService.class);
 
-        userService.add(new User("User1", "Lastname1", "user1@mail.ru",new Car("car_user1", 1)));
-
-        userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("car_user2", 1)));
-        userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("car_user3", 1)));
-        userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("car_user4", 1)));
+//        userService.add(new User("User1", "Lastname1", "user1@mail.ru",new Car("car_user1", 1)));
+//        userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("car_user2", 1)));
+//        userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("car_user3", 1)));
+//        userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("car_user4", 1)));
 
         List<User> users = userService.listUsers();
         for (User user : users) {
@@ -27,7 +25,7 @@ public class MainApp {
             System.out.println("First Name = " + user.getFirstName());
             System.out.println("Last Name = " + user.getLastName());
             System.out.println("Email = " + user.getEmail());
-            System.out.println("Car = " + user.getCar());
+            System.out.println("Car: model - " + user.getCar().getModel()+", series - "+user.getCar().getSeries());
             System.out.println();
         }
 
